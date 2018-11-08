9b9b6e8881d5d242ca33f09d59fa79455e3b12e9
John Solitario <johnsolitario@johns-mbp-9.lan>
2018-11-07 21:21:30 -0800

    Added functionality to the basic analyze graph

--- BasicGraphAnalysis.py
+++ BasicGraphAnalysis.py
@@ -15,8 +15,22 @@ import argparse
 
 
 def analyzeGraph(G):
-## TODO: Basic analysis code here
-
+    nodeCount = G.GetNodes()
+    edgeCount = G.GetEdges()
+    print "Number of nodes in the network: ", nodeCount
+    print "Number of Edges in the network: ", edgeCount
+    print "Graph Density: ", (2.0*edgeCount)/(nodeCount*(nodeCount-1))
+    print "Cluster Coefficient: ", snap.GetClustCf(G)
+        
+#            Components = snap.TCnComV()
+#            snap.GetWccs(G, Components)
+#            number_of_wccs = 0
+#            for comp in Components: number_of_wccs+= 1
+#            print "Number of weakly connected components: ", number_of_wccs
+#            MaxWcc = snap.GetMxWcc(G)
+#            print "Number of Nodes in the largest weakly connected component:", MaxWcc.GetNodes()
+#            print "Number of Edges in the largest weakly connected component: ", MaxWcc.GetEdges()
+#            print "Cluster Coefficient: ", snap.GetClustCf(G)
 	return
 
 
